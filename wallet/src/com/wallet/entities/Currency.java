4781de385a012f23e9889d02895a328e67751baa
Lovatiana15 <hei.lovatiana@gmail.com>
2023-11-30 18:38:53 +0200

    feat: Adds the currency class to the application.

---
+++ wallet/src/com/wallet/entities/Currency.java
@@ -0,0 +1,27 @@
+package com.wallet.entities;
+public class Currency {
+    private int id;
+    private String currencyName;
+    public Currency(int id, String currencyName){
+        this.id = id;
+        this.currencyName = currencyName;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getCurrencyName(){
+        return currencyName;
+    }
+    public int setId(){
+        return id;
+    }
+    public String setCurrencyName(){
+        return currencyName;
+    }
+    @Override
+    public String toString(){
+        return "currency:\n" +
+                "id= " + id + "\n" +
+                "currency: " + currencyName;
+    }
+}
