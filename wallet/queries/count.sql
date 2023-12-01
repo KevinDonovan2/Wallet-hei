c98a72500ae40c17a1889ac07ab924307d8ee182
Lovatiana15 <hei.lovatiana@gmail.com>
2023-12-01 10:18:56 +0200

    fix: add new attribut to the table count

--- wallet/queries/count.sql
+++ wallet/queries/count.sql
@@ -1,7 +1,9 @@
 create table if not exists count (
     id serial primary key,
+    bankName varchar(50) not null,
     username varchar(50),
-    accoutType varchar(50),
+    countNumber varchar(255) not null,
+    accoutType varchar(50) not null,
     balance float,
     idCurrency int references currency(id)
 );
