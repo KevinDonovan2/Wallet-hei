1280cc81fa15aaa87b0f1d9391f4513db1e67c2a
Lovatiana Soa <116557174+Lovatiana15@users.noreply.github.com>
2023-12-06 21:42:37 +0200

    feat: create script SQL for the three table

--- wallet/docs/queries/create_table_currency.sql
+++ wallet/docs/queries/create_table_currency.sql
@@ -1,4 +1,6 @@
-create table if not exists currency(
-    idCurrency serial primary key,
-    currencyName varchar(50)
+DROP TABLE IF EXISTS;
+CREATE TABLE IF NOT EXISTS currency(
+    currencyId serial primary key,
+    currencyName varchar(20) not null,
+    currencyCode varchar(10) CHECK (currencyCode IN ('EUR', 'MGA')) not null
 );
